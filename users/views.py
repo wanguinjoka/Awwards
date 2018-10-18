55f92572509363c46cae32378bbb6d7cfedd4d38
wangui.njoka <wangui.njoka@gmail.com>
2018-10-18 20:18:35 +0300

    able to register new user successfully in the front end

--- users/views.py
+++ users/views.py
@@ -12,7 +12,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}! You are now able to login')
-            return redirect('projects-home')
+            return redirect('home')
 
     else:
         form = UserRegisterForm()
