745deb23e55120884988b213a4e9b24e9e48ada0
wangui.njoka <wangui.njoka@gmail.com>
2018-10-18 19:31:24 +0300

    displaying sites from database on home page

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Site
 
 # Create your views here.
 def home(request):
-
-    return render(request,'projects/home.html')
+    context= {
+         'sites': Site.objects.all()
+         }
+    return render(request,'projects/home.html', context)
