cb1546b6e7acf05456cd4086f2b20ac0fda2a479
wangui.njoka <wangui.njoka@gmail.com>
2018-10-18 19:02:13 +0300

    installed pillow and connected media to settings

--- awwards/settings.py
+++ awwards/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap4',
     'projects.apps.ProjectsConfig',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -123,3 +124,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Site(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/')
+    date_posted = models.DateTimeField(default=timezone.now)
+    site_url = models.CharField(max_length=100, blank=True)
+    developer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
