57158ad17021d1ce2f58dcfeb192158ec29aa015
wangui.njoka <wangui.njoka@gmail.com>
2018-10-19 11:14:14 +0300

    added list view which is a class based view in projects views.py

--- projects/urls.py
+++ projects/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
+from .views import SiteListView
+#, SiteDetailView, SiteCreateView, SiteUpdateView,SiteDeleteView
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
-    url('^$',views.home,name = 'home'),
+    url('^$',SiteListView.as_view(),name = 'home'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+#, DetailView,CreateView,UpdateView, DeleteView
 from .models import Site
 
 # Create your views here.
@@ -7,3 +9,9 @@ def home(request):
          'sites': Site.objects.all()
          }
     return render(request,'projects/home.html', context)
+
+class SiteListView(ListView):
+    model = Site
+    template_name = 'projects/home.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'sites'
+    ordering = ['-date_posted']
