b52a35cadb23caff9ceb7d5848e61883e68dcf42
wangui.njoka <wangui.njoka@gmail.com>
2018-10-19 12:55:40 +0300

    able to upload website using use front end

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView,CreateView,UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView
+#,UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Site
 
 # Create your views here.
